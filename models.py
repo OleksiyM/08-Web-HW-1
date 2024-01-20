afa4c7b926a1a0ab66f0e54a07a2eca7b588e836
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-01-20 23:14:46 +0200

    models.py added

---
+++ models.py
@@ -0,0 +1,36 @@
+from bson import json_util
+from mongoengine import connect, Document, StringField, ListField, ReferenceField, CASCADE
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+
+
+URI = f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/?retryWrites=true&w=majority'
+
+
+# uri = "mongodb+srv://<username>:<password>@cluster0.vhirvg8.mongodb.net/?retryWrites=true&w=majority"
+
+connect(db='Web-HW-8-1', host=URI)
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = StringField()
+    born_location = StringField()
+    description = StringField()
+    meta = {"collection": "authors"}
+
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    quote = StringField(required=True)
+    tags = ListField(StringField(max_length=25))
+    meta = {"collection": "quotes"}
+
